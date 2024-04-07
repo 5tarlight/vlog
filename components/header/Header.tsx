afab6e133326776d59fd5595ddd3001da59e8757
YEAHx4 <developerstarlight@gmail.com>
2024-04-08 00:22:34 +0900

    header content padding in x axis

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,7 +4,7 @@ import LightBulb from "./LightBulb";
 const Header = () => {
   return (
     <header className="flex h-16 justify-center border-b-gray-200 border-b-2 mb-16 w-full">
-      <div className="w-full max-w-[1200px] flex justify-between h-full items-center">
+      <div className="w-full max-w-[1200px] flex justify-between h-full items-center px-4">
         <h1 className="text-[2rem]">
           <Link href="/" className="text-black select-none">
             YEAHx4
