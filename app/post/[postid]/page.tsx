33b27787e9811046fc461e88870005e3ffdcb518
YEAHx4 <developerstarlight@gmail.com>
2024-04-13 12:37:50 +0900

    keywords open graph

--- app/post/[postid]/page.tsx
+++ app/post/[postid]/page.tsx
@@ -19,6 +19,7 @@ export async function generateMetadata(
     description: post?.meta.description,
     applicationName: "YEAHx4 BLOG",
     category: "blog",
+    keywords: post?.meta.tags,
     openGraph: {
       title: post?.meta.title,
       images: {
