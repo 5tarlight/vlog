5e58ca80893b255efad03b07b9a8194c16aefd51
YEAHx4 <developerstarlight@gmail.com>
2024-04-22 12:17:44 +0900

    recent posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
+import { postMetas } from "@/api/postMeta";
+import PostBlock from "@/components/post/PostBlock";
+
 export default function Home() {
-  return <h1 className="text-center">Hello World</h1>;
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-[1024px] w-full">
+        <h1 className="text-center mb-16">YEAHx4 Blog</h1>
+        <div>
+          <h2>최신 포스트</h2>
+          <div className="flex flex-wrap">
+            {postMetas
+              .slice(postMetas.length - 3)
+              .map((post, i) => (
+                <PostBlock
+                  title={post.title}
+                  href={`/post/${post.identifier}`}
+                  cover={post.cover}
+                  key={i}
+                />
+              ))
+              .reverse()}
+          </div>
+        </div>
+        <div>
+          <h2>시리즈로 보기</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ components/post/PostBlock.tsx
@@ -0,0 +1,30 @@
+import { getImage } from "@/api/imageCdn";
+import { PostMeta } from "@/api/postMeta";
+import Link from "next/link";
+
+export default function PostBlock({
+  title,
+  href,
+  cover,
+}: {
+  title: string;
+  href: string;
+  cover?: string;
+}) {
+  return (
+    <Link href={href} className="text-black no-underline hover:no-underline">
+      <div className="w-[300px] h-[200px] flex flex-col rounded-lg border-2 border-gray-300 m-4">
+        <div
+          className="w-full h-[80%] rounded-t-md"
+          style={{
+            backgroundImage: `url(${
+              cover || getImage("thumbnail/yeahx4.png")
+            })`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="p-4">{title}</div>
+      </div>
+    </Link>
+  );
+}
