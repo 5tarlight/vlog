d718bbc27a87d0ff1a005db732f777a974570f2e
YEAHx4 <developerstarlight@gmail.com>
2024-04-24 10:22:01 +0900

    Search result

--- api/series.ts
+++ api/series.ts
@@ -9,12 +9,6 @@ export interface Series {
 
 export const series: Series[] = [
   {
-    id: 0,
-    name: "Demo",
-    description: "블로그의 기능을 테스트하기 위한 시리즈",
-    path: "demo",
-  },
-  {
     id: 1,
     name: "Next.js로 블로그 만들기",
     description: "이 블로그를 만드는 여정",
