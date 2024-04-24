81042ba381f7d88680992db6f3c1046940064638
YEAHx4 <developerstarlight@gmail.com>
2024-04-24 10:02:36 +0900

    Redirect to search page

--- components/header/HeaderSearch.tsx
+++ components/header/HeaderSearch.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function HeaderSearch() {
   const [search, setSearch] = useState("");
+  const router = useRouter();
 
-  const searchPost = () => {};
+  const searchPost = () => {
+    router.push(`/search?q=${search}`);
+  };
 
   return (
     <div className="flex">
@@ -15,10 +19,18 @@ export default function HeaderSearch() {
         onChange={(e) => {
           setSearch(e.target.value);
         }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            searchPost();
+          }
+        }}
         className="px-[0.6rem] py-[0.3rem] w-[300px] bg-gray-200 rounded-l-lg focus:outline-none"
         placeholder="검색"
       />
-      <button className="bg-gray-200 rounded-r-lg hover:bg-gray-300 w-8 flex justify-center items-center">
+      <button
+        className="bg-gray-200 rounded-r-lg hover:bg-gray-300 w-8 flex justify-center items-center"
+        onClick={searchPost}
+      >
         <FaSearch />
       </button>
     </div>
