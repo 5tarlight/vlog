3bcf94b9532c5331b2af552e60d6adb4310c5f35
YEAHx4 <developerstarlight@gmail.com>
2024-04-27 00:03:01 +0900

    build verbose

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+echo "Running Next.js build script with npm"
 npm run build
+
+echo "Current directory"
+pwd
+echo "List files in current directory"
+ls -al
+
+echo "Copying post markdown files to build output"
 cp -r ./post /vercel/output/.next/post_md
