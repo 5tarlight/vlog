25e90a341a4c9c7b82aaa4b8c905ce5d0bf312ab
YEAHx4 <developerstarlight@gmail.com>
2024-04-27 00:05:04 +0900

    log files in build folder

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ npm run build
 
 echo "Current directory"
 pwd
-echo "List files in current directory"
-ls -al
+echo "List files in .next"
+ls -la ./.next
+echo "List files in .vercel"
+ls -la ./.vercel
 
 echo "Copying post markdown files to build output"
 cp -r ./post /vercel/output/.next/post_md
