6903a30c9927174999a1d37b767943f3fa9622a6
YEAHx4 <developerstarlight@gmail.com>
2024-07-13 16:31:45 +0900

    Reset next.js project

--- api/imageCdn.ts
+++
@@ -1,3 +0,0 @@
-export const getImage = (path: string) => {
-  return `https://cdn.jsdelivr.net/gh/5tarlight/vlog-image@main/${path}`;
-};
--- components/util/Gap.tsx
+++
@@ -1,3 +0,0 @@
-export default function Gap({ h }: { h: number }) {
-  return <div style={{ height: `${h}rem` }} />;
-}
