e8f2f524ae2688b27aaf30ceae06dc351648e1b8
YEAHx4 <developerstarlight@gmail.com>
2024-07-15 17:58:36 +0900

    Post header style

--- libs/mdx.ts
+++ libs/mdx.ts
@@ -3,27 +3,27 @@ import remarkGfm from "remark-gfm";
 import rehypePrism from "rehype-prism-plus";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeKatex from "rehype-katex";
-import remarkBreaks from "remark-breaks";
+// import remarkBreaks from "remark-breaks";
 import remarkMath from "remark-math";
-import rehypeStringify from "rehype-stringify";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
+// import rehypeStringify from "rehype-stringify";
+// import remarkParse from "remark-parse";
+// import remarkRehype from "remark-rehype";
 import remarkToc from "remark-toc";
 
 export const serializeMdx = async (src: string) => {
   return serialize(src, {
     mdxOptions: {
       remarkPlugins: [
-        remarkParse,
+        // remarkParse,
         remarkMath,
         // remarkRehype,
         remarkGfm,
-        remarkBreaks,
+        // remarkBreaks,
         remarkToc,
       ],
       rehypePlugins: [
         rehypeKatex,
-        rehypeStringify,
+        // rehypeStringify,
         rehypePrism,
         rehypeAutolinkHeadings,
       ],
