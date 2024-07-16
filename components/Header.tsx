29cad9969f73c2ff5af0516288b47fbba1a053fc
YEAHx4 <developerstarlight@gmail.com>
2024-07-16 21:24:48 +0900

    header & footer

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="/" className="header-logo">
+        YEAHx4 Blog
+      </a>
+      <div>Search (TODO)</div>
+    </header>
+  );
+}
