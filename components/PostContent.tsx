6e3aae449acc2c24d5c2b0d040e82933a20ca7f3
YEAHx4 <developerstarlight@gmail.com>
2024-07-22 02:24:10 +0900

    Disable loading skelton

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -3,8 +3,8 @@
 import { serializeMdx } from "@/libs/mdx";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { useEffect, useState } from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
 
 export default function PostContent({ mdx }: { mdx: string }) {
   const [content, setContent] = useState<MDXRemoteSerializeResult>();
@@ -15,7 +15,7 @@ export default function PostContent({ mdx }: { mdx: string }) {
   return (
     <div className="prose dark:prose-dark flex justify-center">
       <div className="container">
-        {content ? <MDXRemote {...content} /> : <Skeleton count={40} />}
+        {content ? <MDXRemote {...content} /> : <div />}
       </div>
     </div>
   );
