a0b56c95f7d8d5cf893647e47e0579e0f38bbef6
YEAHx4 <developerstarlight@gmail.com>
2024-07-21 20:34:17 +0900

    Mock post data

--- app/post/[...slugs]/page.tsx
+++ app/post/[...slugs]/page.tsx
@@ -7,50 +7,58 @@ import { CiCalendar, CiClock2 } from "react-icons/ci";
 import Comment from "@/components/Comment";
 import Tag from "@/components/Tag";
 
-// export async function generateMetadata({
-//   params: { slugs },
-// }: {
-//   params: { slugs: string[] };
-// }) {
-//   const post = getAllPosts().find(
-//     (post) => post.slug.toLowerCase() === slugs.join("/").toLowerCase()
-//   );
+export async function generateMetadata({
+  params: { slugs },
+}: {
+  params: { slugs: string[] };
+}) {
+  const post = getAllPosts().find(
+    (post) => post.slug.toLowerCase() === slugs.join("/").toLowerCase()
+  );
 
-//   return {
-//     title: post?.title || "Not Found",
-//     description: post?.description,
-//     applicationName: "YEAHx4 BLOG",
-//     category: "blog",
-//     keywords: post?.tags,
-//     openGraph: {
-//       title: post?.title,
-//       // images: {
-//       //   url: getImage(post?.meta.cover || "thumbnail/yeahx4bg.png"),
-//       //   alt: "YEAHx4",
-//       //   width: post?.meta.coverWidth || 1280,
-//       //   height: post?.meta.coverHeight || 720,
-//       // },
-//       type: "article",
-//       authors: ["YEAHx4"],
-//     },
-//   };
-// }
+  return {
+    title: post?.title || "Not Found",
+    description: post?.description,
+    applicationName: "YEAHx4 BLOG",
+    category: "blog",
+    keywords: post?.tags,
+    openGraph: {
+      title: post?.title,
+      // images: {
+      //   url: getImage(post?.meta.cover || "thumbnail/yeahx4bg.png"),
+      //   alt: "YEAHx4",
+      //   width: post?.meta.coverWidth || 1280,
+      //   height: post?.meta.coverHeight || 720,
+      // },
+      type: "article",
+      authors: ["YEAHx4"],
+    },
+  };
+}
 
 export default async function Post({
   params,
 }: {
   params: { slugs: string[] };
 }) {
-  const { slugs } = params;
-  const posts = getAllPosts();
-  const post = posts.find(
-    (post) => post.slug.toLowerCase() === slugs.join("/").toLowerCase()
-  );
+  // const { slugs } = params;
+  // const posts = getAllPosts();
+  // const post = posts.find(
+  //   (post) => post.slug.toLowerCase() === slugs.join("/").toLowerCase()
+  // );
 
-  if (!post) {
-    notFound();
-  }
+  // if (!post) {
+  //   notFound();
+  // }
 
+  const post = {
+    title: "title",
+    content: "# content",
+    series: null,
+    tags: ["a", "b"],
+    date: "2021-01-01 12:00:00",
+    readingMinutes: 100,
+  };
   const mdx = serializeMdx(post.content);
 
   return (
