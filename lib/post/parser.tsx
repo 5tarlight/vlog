f594b80a61de8adcc24face8123bdc3e97202d91
YEAHx4 <developerstarlight@gmail.com>
2024-10-20 01:17:35 +0900

    bugfix: ignore # in codeblock

--- lib/post/parser.tsx
+++ lib/post/parser.tsx
@@ -11,13 +11,16 @@ export const parseToc = (content: string): Toc[] => {
   const toc: Toc[] = [];
   const lines = content.split("\n");
 
+  let isInCodeBlock = false;
   for (const line of lines) {
     const match = line.match(/^#{1,6} (.*)$/);
-    if (match) {
+    if (!isInCodeBlock && match) {
       toc.push({
         value: match[1],
         level: match[0].length - match[1].length - 1,
       });
+    } else if (line.startsWith("```")) {
+      isInCodeBlock = !isInCodeBlock;
     }
   }
 
