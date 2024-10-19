96272c4759efc301c739793ba2928b44f241c8b1
YEAHx4 <developerstarlight@gmail.com>
2024-10-19 18:01:02 +0900

    align footer with grid

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,18 +6,20 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className={cn("flex justify-center")}>
+    <footer className={cn("flex justify-center px-4")}>
       <div
         className={cn(
           "max-w-[1024px] w-full border-t border-gray-500",
-          "flex justify-between items-center p-4 h-32"
+          "grid grid-cols-3 p-4 h-32 items-center"
         )}
       >
-        <div>&copy; 2024{year !== 2024 && `~${year}`} YEAHx4.</div>
-        <div className="text-3xl">
+        <div className="justify-self-start">
+          &copy; 2024{year !== 2024 && `~${year}`} YEAHx4.
+        </div>
+        <div className="text-3xl justify-self-center">
           <FaCreativeCommonsBy />
         </div>
-        <div className="flex gap-4 text-3xl">
+        <div className="flex gap-4 text-3xl justify-self-end">
           <Link href="https://github.com/5tarlight/vlog">
             <FaGithub />
           </Link>
