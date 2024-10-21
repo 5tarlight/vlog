35491376abec9143fb60e4e004541b77dc6afced
YEAHx4 <developerstarlight@gmail.com>
2024-10-21 17:30:59 +0900

    series metadata

--- app/series/[slug]/page.tsx
+++ app/series/[slug]/page.tsx
@@ -2,12 +2,37 @@ import PostPreview from "@/components/PostPreview";
 import { parsePost } from "@/lib/post/parser";
 import { getPostsBySeries, readContent, series } from "@/lib/post/posts";
 import Link from "next/link";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const seriesData = series[slug];
+
+  if (!seriesData) {
+    return {
+      title: "Not Found",
+      description: "Not Found",
+    };
+  }
+
+  return {
+    title: seriesData.name + " | 시리즈",
+    description: seriesData.description,
+  };
+}
 
 export default function Series({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
+  if (!series[slug]) {
+    notFound();
+  }
+
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
       <div className="text-center my-16">
