9ccade6acfa4b847b63dde6882a5b9cebdc1f9c4
YEAHx4 <developerstarlight@gmail.com>
2025-01-22 04:01:30 +0900

    feat: PostPreview 가 더 크게 표시되도록 수정

--- components/post-whole/PostSummary.tsx
+++ components/post-whole/PostSummary.tsx
@@ -7,24 +7,23 @@ import Link from "next/link";
 export default function PostSummary({
   meta,
   seriesName,
-  // content,
   id,
 }: {
   id: string;
   meta: PostMeta;
   seriesName?: string;
-  // content: string | ReactNode;
 }) {
   const postUrl = `/posts/${id}`;
 
   return (
     <div
       className={cn(
-        "gap-6 md:gap-6 flex flex-col md:flex-row",
-        "border-y-2 dark:border-gray-500 border-gray-300 py-4 px-2"
+        "gap-4 flex flex-col md:flex-row",
+        "border-y-2 dark:border-gray-500 border-gray-300 py-4 px-2",
+        "transition-all duration-300 ease-in-out"
       )}
     >
-      <div>
+      <div className="flex-shrink-0 w-full md:w-1/3 mb-4 md:mb-0">
         <Link href={postUrl}>
           <Image
             alt={meta.title}
@@ -38,7 +37,7 @@ export default function PostSummary({
       </div>
       <div className="flex flex-col w-full">
         <Link href={postUrl} className="mb-2">
-          <h2 className="font-bold text-2xl">{meta.title}</h2>
+          <h2 className="font-bold text-xl md:text-2xl">{meta.title}</h2>
         </Link>
         <div className="flex gap-2 items-center text-sm text-gray-500 dark:text-gray-300">
           {seriesName && (
@@ -55,11 +54,10 @@ export default function PostSummary({
           <span>{prettifyDate(meta.date)}</span>
         </div>
         <Link href={postUrl}>
-          <p className="text-gray-500 dark:text-gray-300">{meta.description}</p>
+          <p className="mt-2 text-gray-600 dark:text-gray-300">
+            {meta.description}
+          </p>
         </Link>
-        {/* <div className={cn("text-sm", "text-ellipsis overflow-hidden")}>
-          {content}
-        </div> */}
       </div>
     </div>
   );
