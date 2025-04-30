a133e8c20a2d130c64da8482a638483f3651a639
YEAHx4 <developerstarlight@gmail.com>
2025-04-30 17:04:41 +0900

    feat: add search page, adjust search result page design

---
+++ app/search/page.tsx
@@ -0,0 +1,19 @@
+import SearchInput from "@/components/SearchInput";
+
+export default function Search() {
+  return (
+    <div className="w-full mt-64 flex justify-center">
+      <div className="flex flex-col items-center gap-8 px-6 text-center max-w-2xl w-full">
+        <h1 className="text-4xl font-extrabold text-neutral-900 dark:text-neutral-100">
+          포스트를 검색해보세요
+        </h1>
+        <p className="text-lg text-neutral-600 dark:text-neutral-400">
+          궁금한 주제를 입력하거나 키워드, 태그, 글 내용을 검색해보세요.
+        </p>
+        <div className="w-full max-w-xl">
+          <SearchInput />
+        </div>
+      </div>
+    </div>
+  );
+}
