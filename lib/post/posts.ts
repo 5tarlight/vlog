768ca683c1ac5af46852e3bce5532bfd2456707b
YEAHx4 <developerstarlight@gmail.com>
2025-05-13 14:09:04 +0900

    post: temp tree

--- lib/post/posts.ts
+++ lib/post/posts.ts
@@ -82,7 +82,7 @@ export const psGuide = [
   "priority-queue",
   "exponentiation-by-squaring",
   "divide-and-conquer",
-  ...draft([]),
+  ...draft(["tree"]),
 ].map((post) => `ps/${post}`);
 
 export const POST_BASE_PATH = "/posts";
