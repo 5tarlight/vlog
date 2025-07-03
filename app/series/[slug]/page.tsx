de34fb10f392f3049567b0510755a976b0c819ae
YEAHx4 <developerstarlight@gmail.com>
2025-07-03 20:54:04 +0900

    feat: series page remake

--- app/series/[slug]/page.tsx
+++ app/series/[slug]/page.tsx
@@ -5,16 +5,12 @@ import { getPostsBySeries, readContent, series } from "@/lib/post/posts";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata(
-  props: {
-    params: Promise<{ slug: string }>;
-  }
-) {
+export async function generateMetadata(props: {
+  params: Promise<{ slug: string }>;
+}) {
   const params = await props.params;
 
-  const {
-    slug
-  } = params;
+  const { slug } = params;
 
   const seriesData = series[slug];
 
@@ -31,48 +27,46 @@ export async function generateMetadata(
   };
 }
 
-export default async function Series(
-  props: {
-    params: Promise<{ slug: string }>;
-  }
-) {
+export default async function Series(props: {
+  params: Promise<{ slug: string }>;
+}) {
   const params = await props.params;
 
-  const {
-    slug
-  } = params;
+  const { slug } = params;
 
   if (!series[slug]) {
     notFound();
   }
 
   return (
-    <Content>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center my-16">
-          <h1 className="font-bold text-2xl text-gray-800 dark:text-gray-100">
-            시리즈
-          </h1>
-          <p className="font-extrabold md:text-4xl text-3xl text-indigo-600 dark:text-indigo-300">
-            {series[slug].name}
-          </p>
-        </div>
+    <Content className="mt-32">
+      <div className="text-center mb-20">
+        <h1 className="text-4xl font-extrabold tracking-tight text-neutral-900 dark:text-neutral-100 mb-4">
+          {series[slug].name}
+        </h1>
+        <p className="text-2xl md:text-3xl font-medium text-neutral-600 dark:text-neutral-400">
+          {series[slug].description}
+        </p>
+      </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {getPostsBySeries(slug)
-            .map(async (post) => ({
-              meta: parsePost(await readContent(post)).meta,
-              post,
-            }))
-            .map(async (p, i) => {
-              const { meta, post } = await p;
-              return (
-                <Link href={`/posts/${post}`} key={i}>
-                  <PostPreview meta={meta} />
-                </Link>
-              );
-            })}
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+        {(
+          await Promise.all(
+            getPostsBySeries(slug).map(async (post) => {
+              const meta = parsePost(await readContent(post)).meta;
+              return { meta, post };
+            })
+          )
+        ).map(({ meta, post }, i) => (
+          <div
+            key={i}
+            className="w-full max-w-sm p-2 rounded-xl transition-transform hover:scale-[1.025]"
+          >
+            <Link href={`/posts/${post}`}>
+              <PostPreview meta={meta} />
+            </Link>
+          </div>
+        ))}
       </div>
     </Content>
   );
